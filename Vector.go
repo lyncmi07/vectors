5a6c0afb3cb519733834cb477f633cbcd36a575c
Michael Lynch <lyncmi07@yahoo.co.uk>
2018-06-10 01:38:16 +0100

    added distance calculator

--- Vector.go
+++ Vector.go
@@ -1,40 +1,49 @@
-package vectors;
+package vectors
 
-
+import (
+	"math"
+)
 
 type Vector2 interface {
-  IntVal() (int,int);
-  FloatVal() (float64, float64);
-  Add(v Vector2) Vector2;
-  Sub(v Vector2) Vector2;
-  Muli(s int) Vector2;
-  Mulf(s float64) Vector2;
-  Abs() Vector2;
-  Normalise() Vector2f;
-  Magnitude() float64;
-  Right() Vector2;
-  Left() Vector2;
-  Back() Vector2;
+	IntVal() (int, int)
+	FloatVal() (float64, float64)
+	Add(v Vector2) Vector2
+	Sub(v Vector2) Vector2
+	Muli(s int) Vector2
+	Mulf(s float64) Vector2
+	Abs() Vector2
+	Normalise() Vector2f
+	Magnitude() float64
+	Right() Vector2
+	Left() Vector2
+	Back() Vector2
 }
 
 func IntVector(v Vector2) Vector2i {
-  x,y := v.IntVal();
+	x, y := v.IntVal()
 
-  vn := Vector2i{
-    X:x,
-    Y:y,
-  }
+	vn := Vector2i{
+		X: x,
+		Y: y,
+	}
 
-  return vn
+	return vn
 }
 
 func FloatVector(v Vector2) Vector2f {
-  x,y := v.FloatVal();
+	x, y := v.FloatVal()
 
-  vn := Vector2f {
-    X:x,
-    Y:y,
-  }
+	vn := Vector2f{
+		X: x,
+		Y: y,
+	}
 
-  return vn;
+	return vn
+}
+
+func Distance(v1 Vector2, v2 Vector2) float64 {
+	x1, y1 := v1.FloatVal()
+	x2, y2 := v2.FloatVal()
+
+	return math.Sqrt(math.Pow(x1-x2, 2) + math.Pow(y1-y2, 2))
 }
--- Vector2f.go
+++ Vector2f.go
@@ -1,97 +1,99 @@
-package vectors;
+package vectors
 
 import (
-  "math"
+	"math"
 )
 
 type Vector2f struct {
-  X float64;
-  Y float64;
+	X float64
+	Y float64
 }
 
-func (v Vector2f) IntVal() (int,int) {
-  return int(v.X), int(v.Y);
+func (v Vector2f) IntVal() (int, int) {
+	return int(v.X), int(v.Y)
 }
 
-func (v Vector2f) FloatVal() (float64,float64) {
-  return v.X, v.Y
+func (v Vector2f) FloatVal() (float64, float64) {
+	return v.X, v.Y
 }
 
 func (vo Vector2f) Add(v Vector2) Vector2 {
-  x, y := v.FloatVal();
+	x, y := v.FloatVal()
 
-  vo.X += x;
-  vo.Y += y;
+	vo.X += x
+	vo.Y += y
 
-  return vo;
+	return vo
 }
 
 func (vo Vector2f) Sub(v Vector2) Vector2 {
-  x, y := v.FloatVal();
+	x, y := v.FloatVal()
 
-  vo.X -= x;
-  vo.Y -= y;
+	vo.X -= x
+	vo.Y -= y
 
-  return vo;
+	return vo
 }
 
 func (v Vector2f) Abs() Vector2 {
-  v.X = math.Abs(v.X);
-  v.Y = math.Abs(v.Y);
+	v.X = math.Abs(v.X)
+	v.Y = math.Abs(v.Y)
 
-  return v;
+	return v
 }
 
 func (v Vector2f) Muli(s int) Vector2 {
-  v.X *= float64(s);
-  v.Y *= float64(s);
+	v.X *= float64(s)
+	v.Y *= float64(s)
 
-  return v;
+	return v
 }
 
 func (v Vector2f) Mulf(s float64) Vector2 {
-  v.X *= s;
-  v.Y *= s;
+	v.X *= s
+	v.Y *= s
 
-  return v;
+	return v
 }
 
 func (v Vector2f) Normalise() Vector2f {
-  x := math.Abs(v.X);
-  y := math.Abs(v.Y);
+	x := math.Abs(v.X)
+	y := math.Abs(v.Y)
 
-  xn := v.X / (x + y);
-  yn := v.Y / (x + y);
+	xn := v.X / (x + y)
+	yn := v.Y / (x + y)
 
-  vn := Vector2f{X:xn, Y:yn};
+	vn := Vector2f{X: xn, Y: yn}
 
-  return vn;
+	return vn
 }
 
 func (v Vector2f) Magnitude() float64 {
-  x := math.Abs(v.X);
-  y := math.Abs(v.Y);
+	x := math.Abs(v.X)
+	y := math.Abs(v.Y)
 
-  return x + y;
+	return x + y
 }
 
 func (v Vector2f) Left() Vector2 {
-  v.X = v.Y
-  v.Y = -v.X
+	originalX := v.X
+	v.X = v.Y
+	v.Y = -originalX
 
-  return v;
+	return v
 }
 
 func (v Vector2f) Right() Vector2 {
-  v.X = -v.Y
-  v.Y = v.X
+	originalX := v.X
+	v.X = -v.Y
+	v.Y = originalX
 
-  return v;
+	return v
 }
 
 func (v Vector2f) Back() Vector2 {
-  v.X = -v.X;
-  v.Y = -v.Y;
+	v.X = -v.X
+	v.Y = -v.Y
 
-  return v;
+	return v
 }
